50d08479495d2738c539169923fcc8e7c61d4d23
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2023-03-28 16:04:05 +0100

    Explaining structure at Routers

--- 01draft/api/languages/III.lang.router.js
+++ 01draft/api/languages/III.lang.router.js
@@ -31,30 +31,41 @@ const router = require('express').Router();
 const { checkToken } = require('../../auth/auth');
 // unregistered users can only see informationbut not change or add it
 
+/// General Languages Related Operations
 router.post("/", createLang);
-router.post("/requests", createLangRequests);
 router.patch("/:id(\\d+)", updateLangByID);
 router.patch("/:isoCode", updateLangByISO);
-router.patch("/requests/:id",updateRequestsByID);
 router.get("/", showAll);
 router.get("/:id(\\d+)", showLangByID);
 router.get("/:isoCode(\[a-zA-Z]{3})", showLangByISO);
-router.get("/requests/:id(\\d+)", showLangRequestsByID);
-router.get("/requests", showAllRequests);
+router.delete("/:id(\\d+)", deleteLangByID);
+router.delete("/:isoCode(\[a-zA-Z]{3})", deleteLangByISO);
+
+/// Searching by Alternative Name
 router.get("/alt_name/:alt_name", showLangByAltName);
+
+/// See all dialects
 router.get("/dialects", showAllDialects);
+
+/// Regional Ones
 router.get("/region/:region_name", showLangByRegion);
 router.get("/subregion/:subregion_name", showLangBySubregion);
 router.get("/intregion/:intregion_name", showLangByIntregion);
 router.get("/country/:country", showLangByCountry); // works for only English names
+
+/// Show Countries
 router.get("/showCountries/:lang", showCountriesByLanguage);
+
+/// Request related
+router.post("/requests", createLangRequests);
+router.patch("/requests/:id",updateRequestsByID);
+router.get("/requests/:id(\\d+)", showLangRequestsByID);
+router.get("/requests", showAllRequests);
 router.get("/requests/complete", showAllCompleteRequests);
 router.get("/requests/complete/:id(\\d+)", showCompleteRequestByLang);
 router.get("/requests/open", showAllOpenRequests);
 router.get("/requests/open/:id(\\d+)", showOpenRequestByLang);
 router.get("/requests/pending", showAllPendingRequests);
 router.get("/requests/pending/:id(\\d+)", showPendingRequestsByLang);
-router.delete("/:id(\\d+)", deleteLangByID);
-router.delete("/:isoCode(\[a-zA-Z]{3})", deleteLangByISO);
 
 module.exports = router;
--- 01draft/api/users/III.user.router.js
+++ 01draft/api/users/III.user.router.js
@@ -5,18 +5,21 @@ const router = require('express').Router();
 const { checkToken } = require('../../auth/auth');
 // unregistered users can only see informationbut not change or add it
 
+/// General user operations
 router.post("/", checkToken, createUser);
 router.patch("/:id", checkToken, updateUser);
 router.delete("/:id(\\d+)", checkToken, deleteUser);
 
+/// Showing operations
 router.get("/show", showAllUsers);
 router.get("/show/:id(\\d+)", showUserByID);
 router.get("/show/:email", showUserByEmail);
 
+/// Operation for testing
 router.delete("/testing/:user_id(\\d+)", checkToken, deleteTestHistory);
 
+/// Login operations
 router.post("/login", login);
 router.post("/login/:user_id", logout);
-// add something for language request history? suggested: /API/languages/history/
 
 module.exports = router;
